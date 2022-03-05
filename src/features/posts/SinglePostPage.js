19d0a70dbebfb0ac2c63d7ff183fd96664e2c6e9
John-Yip-HK <yip4work@gmail.com>
2022-03-05 16:29:00 +0800

    Showing Single Posts

---
+++ src/features/posts/SinglePostPage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+// match object contains the URL information we're looking for.
+const SinglePostPage = ({ match }) => {
+  const { postId } = match.params
+
+  // The component will re-render any time the value returned from useSelector changes to a new reference.
+  const post = useSelector((state) =>
+    state.posts.find((post) => post.id === postId)
+  )
+
+  return (
+    <section>
+      {!post ? (
+        <section>
+          <h2>Post not found!</h2>
+        </section>
+      ) : (
+        <section>
+          <article className="post">
+            <h2>{post.title}</h2>
+            <p className="post-content">{post.content}</p>
+          </article>
+        </section>
+      )}
+    </section>
+  )
+}
+
+export default SinglePostPage
