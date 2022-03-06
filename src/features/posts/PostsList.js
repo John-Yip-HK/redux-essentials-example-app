f401ade04167ec6859f86c66efb25c1fefb693ab
John-Yip-HK <yip4work@gmail.com>
2022-03-06 11:32:06 +0800

    Displaying Loading Actions

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -5,43 +5,58 @@ import { PostAuthor } from './PostAuthor'
 import { ReactionButtons } from './ReactionButtons'
 import { TimeAgo } from './TimeAgo'
 import { selectAllPosts, fetchPosts } from './postsSlice'
-
+import { Spinner } from '../../components/Spinner'
 // All of the code related to our feed posts feature should go in the posts folder
+
+const PostExcerpt = ({ post }) => (
+  <article className="post-excerpt" key={post.id}>
+    <h3>{post.title}</h3>
+    <div>
+      <PostAuthor userId={post.user} />
+      <TimeAgo timestamp={post.date} />
+    </div>
+    <p className="post-content">{post.content.substring(0, 100)}</p>
+
+    <ReactionButtons post={post} />
+    <Link to={`/posts/${post.id}`} className="button muted-button">
+      View Post
+    </Link>
+  </article>
+)
 
 const PostsList = () => {
   const dispatch = useDispatch()
   const posts = useSelector(selectAllPosts)
 
   const postStatus = useSelector((state) => state.posts.status)
+  const error = useSelector((state) => state.posts.error)
 
   useEffect(() => {
     if (postStatus === 'idle') {
       dispatch(fetchPosts())
     }
   }, [postStatus, dispatch])
-  const orderedPosts = posts
-    .slice()
-    .sort((a, b) => b.date.localeCompare(a.date))
 
-  const renderedPosts = orderedPosts.map((post) => (
-    <article className="post-excerpt" key={post.id}>
-      <h3>{post.title}</h3>
-      <p className="post-content">{post.content.substring(0, 100)}</p>
-      <div>
-        <PostAuthor userId={post.userId} />
-        <TimeAgo timestamp={post.date} />
-      </div>
-      <ReactionButtons post={post} />
-      <Link to={`/posts/${post.id}`} className="button muted-button">
-        View Post
-      </Link>
-    </article>
-  ))
+  let content
+
+  if (postStatus === 'loading') {
+    content = <Spinner text="loading..." />
+  } else if (postStatus === 'succeeded') {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date))
+
+    content = orderedPosts.map((post) => (
+      <PostExcerpt key={post.id} post={post} />
+    ))
+  } else if (postStatus === 'failed') {
+    content = <div>{error}</div>
+  }
 
   return (
     <section className="posts-list">
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   )
 }
