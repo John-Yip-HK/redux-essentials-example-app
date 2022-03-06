5326220b9e4f556e3f85185b9ef90d262fa303ef
John-Yip-HK <yip4work@gmail.com>
2022-03-06 11:03:31 +0800

    Reducers and Loading Actions

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -64,6 +64,20 @@ const postsSlice = createSlice({
         existingPost.reactions[reaction]++
       }
     },
+    extraReducers: (builder) => {
+      builder
+        .addCase(fetchPosts.pending, (state, action) => {
+          state.status = 'loading'
+        })
+        .addCase(fetchPosts.fulfilled, (state, action) => {
+          state.status = 'succeeded'
+          state.posts = state.posts.concat(action.payload)
+        })
+        .addCase(fetchPosts.failed, (state, action) => {
+          state.status = 'failed'
+          state.error = action.error.message
+        })
+    },
   },
 })
 
