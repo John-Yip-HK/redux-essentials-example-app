6106da885cc7b8e1006c6c8232a0437bd0911e3b
John-Yip-HK <yip4work@gmail.com>
2022-03-06 13:45:51 +0800

    Adding Notifications
    Thunk Arguments

--- src/features/notifications/notificationsSlice.js
+++ src/features/notifications/notificationsSlice.js
@@ -2,6 +2,8 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
 import { client } from '../../api/client'
 
+// For the payload creator, the first argument of the callback is the argument developers passed in.
+// The second one is a thunkAPI object with some useful functions and information. getState allows developers to retrieve state of this slice.
 export const fetchNotifications = createAsyncThunk(
   'notifications/fetchNotifications',
   async (_, { getState }) => {
